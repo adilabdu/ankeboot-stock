7e246df4087e95fd6c88e8f1cc4c97055b92546f
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-11-20 23:00:11 +0900

    New Controller, Migration and Model classes for Book and Stock

---
+++ database/migrations/2020_11_20_134629_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('author')->nullable();
+            $table->string('published_year')->nullable();
+            $table->string('ISBN')->unique()->nullable();
+            $table->double('cost_price');
+            $table->double('selling_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
