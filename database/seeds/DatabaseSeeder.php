a6757d82d5912fc61eaae19e583b91ca7532f34e
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-12-05 13:05:49 +0900

    Added Database Seeder for Books

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create();
+        foreach(range(1,10) as $indx) {
+            DB::table('books')->insert([
+                'name' => $faker->words(3),
+                'author' => $faker->firstName . " " . $faker->lastName,
+                'published_year' => $faker->year,
+                'ISBN' => $faker->isbn13,
+                'cost_price' => $faker->randomFloat(2, 1, 50),
+                'selling_price' => $faker->randomFloat(2, 1, 50) + 7.49,
+            ]);
+        }
     }
 }
