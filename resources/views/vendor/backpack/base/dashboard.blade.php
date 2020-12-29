81ae2eb0747556e2359095cfbd52993467f36841
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-12-30 00:39:00 +0900

    Handled division by 0 error

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -25,7 +25,11 @@
         $averageBalance += $book->balance();
         $maxBalance = max($maxBalance, $book->balance());
     }
-    $averageBalance = $averageBalance / $booksCount;
+    if($booksCount > 0) {
+        $averageBalance = $averageBalance / $booksCount;
+    } else {
+        $averageBalance = 'N/A';
+    }
 
     $articleCount = 100;
     $lastArticleDaysAgo = 3;
@@ -40,8 +44,7 @@
 			->progressClass('progress-bar')
 			->value($booksCount)
 			->description('Registered books.')
-			->progress($consignment / $booksCount * 100)
-			->hint('<span class="text-white">' . $consignment . ' consignments, </span> ' . $cheques . ' cheques'),
+			->hint('<span class="text-white">' . $consignment . ' consignments, </span> ' . $cheques . ' cash'),
 
 		// alternatively, to use widgets as content, we can use the same add() method,
 		// but we need to use onlyHere() or remove() at the end
@@ -49,10 +52,10 @@
 		    ->type('progress')
 		    ->class('card border-0 text-white bg-success')
 		    ->progressClass('progress-bar')
-		    ->value($latestBook->name)
+		    ->value($latestBook ? $latestBook->name : 'N/A')
 		    ->description('Last Registered Book')
 		    // ->progress(80)
-		    ->hint('Added On: <span class="text-white">' . $latestBook->created_at . '</span>')
+		    ->hint('Added On: <span class="text-white">' . ($latestBook ? $latestBook->created_at : 'N/A') . '</span>')
 		    ->onlyHere(),
 
 		// alternatively, you can just push the widget to a "hidden" group
@@ -60,11 +63,11 @@
 			->group('hidden')
 		    ->type('progress')
 		    ->class('card border-0 text-white bg-warning')
-		    ->value(floor($averageBalance))
+		    ->value($averageBalance == 'N/A' ? 'N/A' : floor($averageBalance))
 		    ->progressClass('progress-bar')
 		    ->description('Average Balance of Books in Inventory')
 		    // ->progress(30)
-		    ->hint('Maximum Balance: <span class="text-white">' . $maxBalance . '</span>'),
+		    ->hint('Highest Balance: <span class="text-white">' . $maxBalance . '</span>'),
 
 		// both Widget::make() and Widget::add() accept an array as a parameter
 		// if you prefer defining your widgets as arrays
@@ -73,8 +76,7 @@
 			'class'=> 'card border-0 text-white bg-dark',
 			'progressClass' => 'progress-bar',
 			'value' => $productCount,
-			'description' => 'Products.',
-			'progress' => (int)$productCount/75*100,
+			'description' => 'Another Statistic.',
 			'hint' => $productCount>75?'Try to stay under 75 products.':'Good. Good.',
 		]),
 	]);
