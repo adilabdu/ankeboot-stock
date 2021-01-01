3dbc46e524a0650e6bcd1e02349beba88d7a3a4c
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-01-02 00:27:30 +0900

    Navigation links updated on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ankeboot Stocks Manager</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -72,8 +72,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="/docs.html">Documentation</a>
-                    <a href="/admin">Dashboard</a>
+                    <a href="ankeboot-stock/docs.html">Documentation</a>
+                    <a href="{{ backpack_url('') }}">Dashboard</a>
                 </div>
             </div>
         </div>
